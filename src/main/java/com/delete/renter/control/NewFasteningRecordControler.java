beb2bc0a13924db683a69a5ced17effc87063819
席晶晶 <hzxijingjing@corp.netease.com>
2019-12-21 16:00:55 +0800

    统一价格

--- src/main/java/com/delete/renter/control/NewFasteningRecordControler.java
+++ src/main/java/com/delete/renter/control/NewFasteningRecordControler.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 import org.apache.commons.lang3.StringUtils;
 
 import java.net.URL;
@@ -50,6 +51,12 @@ public class NewFasteningRecordControler implements Initializable {
     @FXML
     private HBox priceHbox;
 
+    @FXML
+    private Button fasteningCancel;
+
+    @FXML
+    private Label msg1;
+
     private final ToggleGroup group = new ToggleGroup();
 
     public void onFasteningAddSave(){
@@ -57,35 +64,41 @@ public class NewFasteningRecordControler implements Initializable {
     }
     
     public void onFasteningAddCancel(){
-        System.out.println("");
+        Stage stage = (Stage)fasteningCancel.getScene().getWindow();
+        stage.close();
     }
 
     @FXML
     public void onReturn(){
         x.setVisible(false);
         unitprice.setVisible(false);
-        priceHbox.setVisible(false);
+        msg1.setVisible(false);
     }
 
     @FXML
     public void onRenter(){
         x.setVisible(true);
         unitprice.setVisible(true);
-        priceHbox.setVisible(true);
+        msg1.setVisible(false);
     }
 
     public void initNorms(){
         ObservableList<String> tmpList = FXCollections.observableArrayList();
-        tmpList.add("规格1");
-        tmpList.add("规格2");
-        tmpList.add("规格3");
+        tmpList.add("扣件1");
+        tmpList.add("扣件2");
+        tmpList.add("扣件3");
         norms.setItems(tmpList);
         norms.setValue("请选择规格");
     }
 
     public void onAddNorm(){
         String srcText = curNorms.getText();
-        String normsStr = norms.getValue() + ":" + number.getText() + "*" + unitprice.getText();
+        if(StringUtils.isEmpty(number.getText()) || StringUtils.isEmpty(norms.getValue())){
+            msg1.setVisible(true);
+            return;
+        }
+        msg1.setVisible(false);
+        String normsStr = norms.getValue() + "*" + number.getText();
         if (StringUtils.isEmpty(srcText)){
             curNorms.setText(normsStr);
         }else{
@@ -103,5 +116,6 @@ public class NewFasteningRecordControler implements Initializable {
         returnType.setToggleGroup(group);
         renterType.setToggleGroup(group);
         initNorms();
+        msg1.setVisible(false);
     }
 }
